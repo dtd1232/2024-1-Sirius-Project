1648daf2c052a6201cd65dad436bc09cbc6f5041
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-04-07 20:43:07 +0900

    Feat: 입력 시스템 구현 (#11)
    * Feat: 입력 시스템 구현
    
    * Feat: getText 추가, saveText을 private로 전환

---
+++ Assets/Scripts/DH/inputHandling.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using TMPro;
+
+public class inputHandling : MonoBehaviour
+{
+
+    private string input;
+    [SerializeField]
+    private TMP_InputField inputField;
+
+    void Update()
+    {
+        inputField.ActivateInputField();
+        
+
+        if (Input.GetKeyDown(KeyCode.Return) ){
+            saveText();
+            inputField.text = "";
+        }
+    }
+
+    private void saveText()
+    {
+        this.input = inputField.text;
+        Debug.Log("Input Text: " + input);
+    }
+
+    public string getText()
+    {
+        return this.input;
+    }
+}
