2e7301e2788da713710169e5d5d84969c8da00e7
khlee8688 <hosanna62100@gmail.com>
2024-04-12 23:00:05 +0900

    Fix: 48개의 오류 해결

--- Assets/Scripts/KH/LongerCoin.cs
+++ Assets/Scripts/KH/LongerCoin.cs
@@ -5,37 +5,43 @@ using UnityEngine;
 public class LongerCoin : MonoBehaviour
 {
     public string input;
-    public long int coin=0;
-    public List<string> passKeyword;    
+    public int coin=0;
+    public List<string> passKeyword;
     [SerializeField] int coinProbability;
-    public GameObject inputHandling;
+    public GameObject handling;
+    private List<string> enemyAction;
     void Start()
     {
-        inputHandling = GameObject.find("Canvas"); //"inputhanling이 있는 canvas/inputhandler" 같은 scene에 존재하거나 gamemanager 비슷한게 있어야 함
+        handling = GameObject.Find("Canvas"); //"inputhanling이 있는 canvas/inputhandler" / 같은 scene에 존재하거나 gamemanager 비슷한게 있어야 함
     }
 
     // Update is called once per frame
     void Update()
     {
-        input = inputHandling.GameObject.GetComponent<inputHandling>.input;
+        input = handling.gameObject.GetComponent<inputHandling>().getText();
     }
 
-    void OncollisionEnter(Collision other){
-        Random randGen = new Random();
-        private int randval;
-        enemyAction = other.gameObject.GetComponent<enemy>().thisPassKeywords.ToList();
+    void OnTriggerEnter2D(Collider2D other){
+        //Random randGen = new Random();
+        int randval;
+        enemyAction = other.gameObject.GetComponent<ObstacleScript>().thisPassKeywords;
         for(int i=0; i<enemyAction.Count; i++){
-            if(string.Compare(input, enemyAction[i], true)==0 && enemyAction.Length()>=2){
-                if(input.Length > enemyAction[(i+1)%2]){
-                    randval = randGen(0, 100);
-                    if(ranval<coinProbability) GetCoin();
+            //대소문자를 무시한 input과 장애물 키워드가 같고, 키워드가 2개 이상이라면 실행됨
+            if(string.Compare(input, enemyAction[i], System.StringComparison.OrdinalIgnoreCase)==0 && enemyAction.Count>=2){
+                //입력한 단어가 다른 단어보다 길이가 길면 실행됨
+                if(input.Length > enemyAction[(i+1)%2].Length){
+                    //0~99의 숫자 중 하나를 불러오고 코인 획득 확률(coinProbability)보다 작으면 getCoin() 실행
+                    //randval = randGen.Next(0, 100);
+                    randval = Random.Range(0, 100);
+                    if(randval<coinProbability) GetCoin();
                 }
             }
         }
-        
     }
 
     void GetCoin(){
         coin++;
+        Debug.Log(coin); // coin 확인용으로 만들어놨음
     }
+
 }
