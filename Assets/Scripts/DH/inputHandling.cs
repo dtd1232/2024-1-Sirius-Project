2df27efe91fa4b3caa035d8fc194564bffe61f65
dtd1232 <dtd12321@gmail.com>
2024-04-12 22:59:43 +0900

    [Feat] Ctrl, Command 입력 감지 및 textField 색 변화

--- Assets/Scripts/DH/inputHandling.cs
+++ Assets/Scripts/DH/inputHandling.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +12,26 @@ public class inputHandling : MonoBehaviour
     [SerializeField]
     private TMP_InputField inputField;
 
+    [SerializeField]
+    private Color originalColor;
+    [SerializeField]
+    private Color alertColor;
+    private IEnumerator coroutine;
+
+    private void Start()
+    {
+        coroutine = copyPasteAlert();
+    }
+
     void Update()
     {
         inputField.ActivateInputField();
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) ||
+            Input.GetKeyDown(KeyCode.LeftCommand) || Input.GetKeyDown((KeyCode.RightCommand)))
+        {
+            StartCoroutine(copyPasteAlert());
+        }
 
         if (Input.GetKeyDown(KeyCode.Return) ){
             saveText();
@@ -31,5 +48,14 @@ public class inputHandling : MonoBehaviour
     public string getText()
     {
         return this.input;
+    }
+
+    IEnumerator copyPasteAlert()
+    {
+        inputField.image.color = alertColor;
+        
+        yield return new WaitForSeconds(1f);
+
+        inputField.image.color = originalColor;
     }
 }
