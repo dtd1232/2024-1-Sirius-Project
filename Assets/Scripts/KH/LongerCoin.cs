d1e99cb613ae0b47e6257d661a332e8a0d17780a
khlee8688 <hosanna62100@gmail.com>
2024-04-14 20:33:34 +0900

    Fix: 불필요한 부분 삭제 및 coin HideInInspector

--- Assets/Scripts/KH/LongerCoin.cs
+++ Assets/Scripts/KH/LongerCoin.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class LongerCoin : MonoBehaviour
 {
     private string input;
-    public int coin=0;
-    private List<string> passKeyword;
     [SerializeField] int coinProbability;
     private GameObject handling;
     private List<string> enemyAction;
+    [HideInInspector]
+    public int coin=0;
     void Start()
     {
-        handling = GameObject.Find("Canvas"); //"inputhanling이 있는 canvas/inputhandler" / 같은 scene에 존재하거나 gamemanager 비슷한게 있어야 함
+        handling = GameObject.Find("Canvas");
     }
 
     // Update is called once per frame
@@ -22,8 +22,6 @@ public class LongerCoin : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("Collider");
-        //Random randGen = new Random();
         int randval;
         enemyAction = other.gameObject.GetComponent<ObstacleScript>().thisPassKeywords;
         for(int i=0; i<enemyAction.Count; i++){
@@ -32,7 +30,6 @@ public class LongerCoin : MonoBehaviour
                 //입력한 단어가 다른 단어보다 길이가 길면 실행됨
                 if(input.Length > enemyAction[(i+1)%2].Length){
                     //0~99의 숫자 중 하나를 불러오고 코인 획득 확률(coinProbability)보다 작으면 getCoin() 실행
-                    //randval = randGen.Next(0, 100);
                     randval = Random.Range(0, 100);
                     if(randval<coinProbability) GetCoin();
                 }
@@ -42,7 +39,6 @@ public class LongerCoin : MonoBehaviour
 
     void GetCoin(){
         coin++;
-        Debug.Log("coin : " + coin); // coin 확인용으로 만들어놨음
     }
 
 }
