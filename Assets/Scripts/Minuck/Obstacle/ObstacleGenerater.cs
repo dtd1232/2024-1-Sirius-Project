76471dbb11635e03eb162869a20cae36e16f403b
AdHoc2675 <alsdnr2675@naver.com>
2024-04-18 20:08:36 +0900

    [Feat] 타 씬과 병합. 처음 등장하는 장애물에 텍스트 동시 생성

--- Assets/Scripts/Minuck/Obstacle/ObstacleGenerater.cs
+++ Assets/Scripts/Minuck/Obstacle/ObstacleGenerater.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ObstacleGenerater : MonoBehaviour
@@ -8,7 +9,7 @@ public class ObstacleGenerater : MonoBehaviour
 
     public List<int> generateCountforEachType;
     [SerializeField] private List<GameObject> obstacleArray;
-    [SerializeField] private List<bool> isItFirstTimeObstacle;
+    [SerializeField] private GameObject keywordGuideText;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,6 @@ public class ObstacleGenerater : MonoBehaviour
         for (int i = 0; i < obstacleArray.Count; i++)
         {
             generateCountforEachType.Add(0);
-            isItFirstTimeObstacle.Add(false);
         }
 
         StartCoroutine(GenerateObstacle());
@@ -33,11 +33,20 @@ public class ObstacleGenerater : MonoBehaviour
             if (obstacleArray.Count > 0)  // 장애물 배열이 비어있지 않은지 확인
             {
                 int num = Random.Range(0, obstacleArray.Count);  // 정확한 랜덤 인덱스 계산
-                Debug.Log(num);
 
                 // 추후 이 부분에서 하드코딩된거 변수화 하거나 위치 조정
-                Instantiate(obstacleArray[num], new Vector3(20.0f, 0.0f, 0.0f), this.transform.rotation);
+                GameObject obstacle = Instantiate(obstacleArray[num], new Vector3(20.0f, 0.0f, 0.0f), this.transform.rotation);
+                
+                // 처음 나오는 Obstacle이면 키워드 알려주는 텍스트 생성
+                if (generateCountforEachType[num] == 0)
+                {
+                    GameObject keywordGuideTextasChild = Instantiate(keywordGuideText, obstacle.transform);
+
+                    keywordGuideTextasChild.transform.localPosition = new Vector3(0, 1.5f, 0);
+                }
                 generateCountforEachType[num]++;
+
+                
             }
         }
     }
