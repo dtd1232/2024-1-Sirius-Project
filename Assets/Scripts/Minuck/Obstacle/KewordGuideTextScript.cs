c23980efaa51636b2acc4bf3e5bd3b2e2201c7a8
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2024-04-28 15:03:16 +0900

    [FEAT#15] 장애물 키워드 표기 (#25)
    * [Docs] 한글 깨짐 문제 해결
    
    인코딩 형식을 바꿔서 한글 깨짐 문제 해결
    
    * [Feat] 타 씬과 병합. 처음 등장하는 장애물에 텍스트 동시 생성
    
    * [Feat] 키워드 확인용 디버그 로그 추가
    
    * [Feat] 첫 생성 오브젝트의 키워드 불러오기
    
    * [Feat] 처음 등장하는 장애물은 키워드를 제공하는 기능 완료
    
    * [Docs] 디버그 로그 부분 코드 비활성화
    
    * [Docs] 각주 및 미사용 코드 삭제

---
+++ Assets/Scripts/Minuck/Obstacle/KewordGuideTextScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KewordGuideTextScript : MonoBehaviour
+{
+
+    private List<string> keywords = new List<string>();
+    private string guideText = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        keywords = GetComponentInParent<ObstacleScript>().thisPassKeywords;
+
+        for (int i  = 0; i < keywords.Count; i++)
+        {
+            guideText = guideText + keywords[i] + "\n";
+        }
+
+        this.GetComponent<TextMeshPro>().text = guideText;
+    }
+}
