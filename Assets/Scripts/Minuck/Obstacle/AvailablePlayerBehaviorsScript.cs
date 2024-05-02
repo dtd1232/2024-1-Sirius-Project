4f2e355279b77a639e24f2a5ed75579a63260dc2
AdHoc2675 <alsdnr2675@naver.com>
2024-05-02 19:41:50 +0900

    [feat] 입력되는 string에 따라, panel이 생성되어 화면에 표시되는 기능 구현

--- Assets/Scripts/Minuck/Obstacle/AvailablePlayerBehaviorsScript.cs
+++ Assets/Scripts/Minuck/Obstacle/AvailablePlayerBehaviorsScript.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AvailablePlayerBehaviorsScript : MonoBehaviour
 {
-    [SerializeField] GameObject AvailablePlayerBehaviorPanelTemplate;
-    [SerializeField] List<GameObject> AvailablePlayerBehaviorPanelList = new List<GameObject>();
+    [SerializeField] private GameObject AvailablePlayerBehaviorPanelTemplate;
+
+    private List<GameObject> AvailablePlayerBehaviorPanelList = new List<GameObject>();
+    private Queue<GameObject> AvailablePlayerBehaviorPanelQueue = new Queue<GameObject>();
+    private int maxCountOfPanalList = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +20,36 @@ public class AvailablePlayerBehaviorsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)) {
+            int rand = Random.Range(0, 10);
+            Debug.Log(rand);
+            UpdateAvailablePlayerBehaviorPanelList(rand.ToString());
+        }
     }
 
-    void UpdateAvailablePlayerBehaviorPanelList()
+    public void UpdateAvailablePlayerBehaviorPanelList(string keyword)
     {
-        for (int i = 0; i < AvailablePlayerBehaviorPanelList.Count; i++)
-        {
+        GameObject newPanel = Instantiate(AvailablePlayerBehaviorPanelTemplate, this.transform, false);
+        newPanel.GetComponentInChildren<TextMeshProUGUI>().text = keyword;
 
+        AvailablePlayerBehaviorPanelQueue.Enqueue(newPanel);
+
+        if (AvailablePlayerBehaviorPanelQueue.Count > maxCountOfPanalList)
+        {
+            GameObject oldPanel = AvailablePlayerBehaviorPanelQueue.Dequeue();
+            Destroy(oldPanel);
         }
+
+        AvailablePlayerBehaviorPanelList.Clear();
+        foreach (GameObject item in AvailablePlayerBehaviorPanelQueue)
+        {
+            AvailablePlayerBehaviorPanelList.Add(item);
+        }
+
+        for (int i = 0; i < AvailablePlayerBehaviorPanelQueue.Count; i++)
+        {
+            AvailablePlayerBehaviorPanelList[i].GetComponent<RectTransform>().localPosition = new Vector3(110 * i, 0, 0);
+        }
+
     }
 }
