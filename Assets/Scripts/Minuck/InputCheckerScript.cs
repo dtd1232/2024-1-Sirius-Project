a03126a84ca93d5261a0fb68aebb93e563e50f89
AdHoc2675 <alsdnr2675@naver.com>
2024-05-02 20:31:56 +0900

    [feat] 입력한 키워드가 맞는지 틀렸는지 구분하고, 그에 따른 Event를 발생시키는 기능 구현

--- Assets/Scripts/Minuck/InputCheckerScript.cs
+++ Assets/Scripts/Minuck/InputCheckerScript.cs
@@ -2,19 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Windows;
 
 public class InputCheckerScript : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField inputField;
-    // Start is called before the first frame update
-    void Start()
+    private string input;
+    [SerializeField] private GameObject inputField;
+    private List<string> enemyAction;
+
+    public UnityEvent CorrectKeywordEvent;
+    public UnityEvent<string> CorrectKeywordEventWithInput;
+    public UnityEvent IncorrectKeywordEvent;
+    public UnityEvent<string> IncorrectKeywordEventWithInput;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        inputField.onEndEdit.AddListener(InputChecker);
+        input = inputField.gameObject.GetComponent<inputHandling>().getText();
+
+        enemyAction = other.gameObject.GetComponent<ObstacleScript>().thisPassKeywords;
+
+        for (int i = 0; i < enemyAction.Count; i++)
+        {
+            if (string.Compare(input, enemyAction[i], System.StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                CorrectKeyword();
+                CorrectKeyword(input);
+            }
+            else
+            {
+                IncorrectKeyword();
+                IncorrectKeyword(input);
+            }
+        }
     }
 
-    public void InputChecker(string input) {
-    
-        
+    private void CorrectKeyword()
+    {
+        CorrectKeywordEvent.Invoke();
+    }
+
+    private void CorrectKeyword(string input)
+    {
+        CorrectKeywordEventWithInput.Invoke(input);
+    }
+
+    private void IncorrectKeyword()
+    {
+        CorrectKeywordEventWithInput.Invoke(input);
+    }
+
+    private void IncorrectKeyword(string input)
+    {
+        IncorrectKeywordEventWithInput.Invoke(input);
     }
 
 }
