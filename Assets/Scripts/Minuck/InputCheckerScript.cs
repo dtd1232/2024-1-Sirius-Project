38f5f11ea8091f9054bf886f8d8df965cc978b71
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2024-05-10 20:52:33 +0900

    [FEAT#26] 현재 입력 / 사용 가능한 플레이어 행동 전체 UI로 표기 (#28)
    * [feat] 키워드 표시용 UI 탬플릿 형성 및 스크립트 마련
    
    * [feat] 입력되는 string에 따라, panel이 생성되어 화면에 표시되는 기능 구현
    
    * [feat] 입력한 텍스트에 따라 화면에 표시되는 키워드 텍스트가 반영되는 기능 구현
    
    * [feat] 입력한 키워드가 맞는지 판별하기 위한 오브젝트 및 스크립트 생성
    
    * [feat] 입력한 키워드가 맞는지 틀렸는지 구분하고, 그에 따른 Event를 발생시키는 기능 구현
    
    * [feat] 입력한 텍스트가 맞거나 틀렸을 때 이벤트 호출, 맞는 입력일 경우 화면에 해결된 키워드 표시
    
    * [Docs] 미사용 코드, 디버깅용 코드 삭제
    
    * [feat] 컴포넌트 레퍼런스 변경
    
    * [feat] 리스트 크기에 제한 제거, 중복된 키워드 표시 불가능하도록 수정

---
+++ Assets/Scripts/Minuck/InputCheckerScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Windows;
+
+public class InputCheckerScript : MonoBehaviour
+{
+    [SerializeField] private string input;
+    [SerializeField] private GameObject inputField;
+    private List<string> enemyAction;
+    private bool isCorrect = false;
+
+    public UnityEvent CorrectKeywordEvent;
+    public UnityEvent<string> CorrectKeywordEventWithInput;
+    public UnityEvent IncorrectKeywordEvent;
+    public UnityEvent<string> IncorrectKeywordEventWithInput;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        input = inputField.GetComponent<inputHandling>().getText();
+        isCorrect = false;
+
+        if (!other.gameObject.TryGetComponent<ObstacleScript>(out var obstacleScriptComponent))
+        {
+            return;
+        }
+
+        enemyAction = obstacleScriptComponent.thisPassKeywords;
+
+        for (int i = 0; i < enemyAction.Count; i++)
+        {
+            if (string.Compare(input, enemyAction[i], System.StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                isCorrect = true;
+            }
+        }
+
+        if (isCorrect)
+        {
+            CorrectKeyword();
+            CorrectKeyword(input);
+        }
+        else
+        {
+            IncorrectKeyword();
+            IncorrectKeyword(input);
+        }
+    }
+
+    private void CorrectKeyword()
+    {
+        CorrectKeywordEvent.Invoke();
+    }
+
+    private void CorrectKeyword(string input)
+    {
+        CorrectKeywordEventWithInput.Invoke(input);
+    }
+
+    private void IncorrectKeyword()
+    {
+        IncorrectKeywordEvent.Invoke();
+    }
+
+    private void IncorrectKeyword(string input)
+    {
+        IncorrectKeywordEventWithInput.Invoke(input);
+    }
+
+}
---
+++ Assets/Scripts/Minuck/Obstacle/AvailablePlayerBehaviorsScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvailablePlayerBehaviorsScript : MonoBehaviour
+{
+    [SerializeField] private GameObject AvailablePlayerBehaviorPanelTemplate;
+    [SerializeField] private InputCheckerScript inputCheckerScript;
+    private List<GameObject> AvailablePlayerBehaviorPanelList = new();
+    private List<string> AvailablePlayerBehaviorStringList = new();
+
+
+    private void Start()
+    {
+        inputCheckerScript.CorrectKeywordEventWithInput.AddListener(UpdateAvailablePlayerBehaviorPanelList);
+    }
+
+    public void UpdateAvailablePlayerBehaviorPanelList(string keyword)
+    {
+        if (!AvailablePlayerBehaviorStringList.Contains(keyword))
+        {
+            AvailablePlayerBehaviorStringList.Add(keyword);
+
+            GameObject newPanel = Instantiate(AvailablePlayerBehaviorPanelTemplate, this.transform, false);
+            newPanel.GetComponentInChildren<TextMeshProUGUI>().text = keyword;
+            AvailablePlayerBehaviorPanelList.Add(newPanel);
+        }
+
+        for (int i = 0; i < AvailablePlayerBehaviorPanelList.Count; i++)
+        {
+            AvailablePlayerBehaviorPanelList[i].GetComponent<RectTransform>().localPosition = new Vector3(110 * i, 0, 0);
+        }
+
+    }
+}
